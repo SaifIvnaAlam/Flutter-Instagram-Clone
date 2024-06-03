8e1f9f44d908eee3dad7a45809d8bcccd5ca864c
saif ivna alam <alam.saifivna@gmail.com>
2024-06-04 01:04:43 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Instagram Clone
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/phaseone/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.phaseone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
